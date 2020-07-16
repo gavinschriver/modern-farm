d6e8c51f33e22bc2298767d0ae79a73cd81476ad
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-16 12:27:37 -0500

    messing around with array methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,5 +12,11 @@ const arrayOfPlants = usePlants()
 
 console.log(arrayOfPlants)
 
+for (const plant of arrayOfPlants) {
+    if (Array.isArray(plant)) {
+      console.log(`You've got corn at row ${(arrayOfPlants.indexOf(plant) + 1)}`)
+    }
+  }
+
 
 
