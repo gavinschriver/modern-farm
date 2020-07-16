6e7cc93699d72277790219330323a15e723341ac
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-16 10:15:15 -0500

    created all individual plant modules

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,21 @@
-import { createPlan } from "./plan.js"
+import {createSoybean} from "./seeds/soybean.js"
+import {createCorn} from "./seeds/corn.js"
+import {createSunflower} from "./seeds/sunflower.js"
+import {createAsparagus} from "./seeds/asparagus.js"
+import {createWheat} from "./seeds/wheat.js"
+import {createPotato} from "./seeds/potato.js"
 
-console.log("Welcome to the main module")
+const soybeanSeed = createSoybean()
+const cornSeed = createCorn()
+const sunflowerSeed = createSunflower()
+const asparagusSeed = createAsparagus()
+const wheatSeed = createWheat()
+const potatoSeed = createPotato()
 
-const yearlyPlan = createPlan()
+console.log(soybeanSeed)
+console.log(cornSeed)
+console.log(sunflowerSeed)
+console.log(asparagusSeed)
+console.log(wheatSeed)
+console.log(potatoSeed)
 
-console.log(yearlyPlan);
